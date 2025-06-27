a4f8188b98383f722f95c36e05e835cdc176bd80
Mansipatle0 <mansipatle0@gmail.com>
2025-06-27 13:13:44 +0530

    Add files via upload

---
+++ EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.employeemanagement.controller;
+
+import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee emp) {
+        return service.save(emp);
+    }
+}
